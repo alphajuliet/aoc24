8f14bd37d221ba6a57c5af38003f7f00ecb486ae
alphajuliet <andrew@alphajuliet.com>
2024-12-01 17:35:17 +1100

    Solve 1.2

--- src/aoc24/day01.clj
+++ src/aoc24/day01.clj
@@ -6,24 +6,37 @@
 (def inputf "data/day01-input.txt")
 
 (defn create-lists
-  "Create two sorted lists from the two columns"
+  "Create two lists from the two columns"
   [lines]
   (->> lines
        (mapv #(str/split % #"\s+"))
        util/T
-       (map #(mapv Integer/parseInt %))
-       (mapv sort)))
+       (map #(mapv Integer/parseInt %))))
 
 (defn part1
   [f]
   (let [[c1 c2] (->> f
                      util/read-lines
-                     create-lists)]
+                     create-lists
+                     (mapv sort))]
     (->> (mapv #(abs (- %1 %2)) c1 c2)
+         (apply +))))
+
+(defn part2
+  [f]
+  (let [[c1 c2] (->> f
+                     util/read-lines
+                     create-lists)
+        f2 (frequencies c2)]
+    (->> c1
+         (mapv #(* % (get f2 % 0)))
          (apply +))))
        
 (comment
   (part1 testf)
-  (part1 inputf))
+  (part1 inputf)
+
+  (part2 testf)
+  (part2 inputf))
 
 ;; The End
