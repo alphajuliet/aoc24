6c29b601da50de81af28e8feb09421c9f9379956
alphajuliet <andrew@alphajuliet.com>
2025-01-05 15:46:16 +1100

    Refactor

--- src/aoc24/day22.clj
+++ src/aoc24/day22.clj
@@ -8,6 +8,11 @@
   [a b]
   (.xor (BigInteger. (str a)) (BigInteger. (str b))))
 
+(defn read-data [f]
+  (->> f
+       util/read-lines
+       (map edn/read-string)))
+
 (defn next-secret
   "Calculate the next secret number via the algorithm"
   [secret]
@@ -26,21 +31,37 @@
 
 (defn iterate-secrets
   [n secret]
-  (last (take (inc n) (iterate next-secret secret))))
+  (->> secret
+       (iterate next-secret)
+       (take (inc n))))
+
+(defn find-opt-seq
+  [secret]
+  (let [ones (->> secret
+                  (iterate-secrets 2000)
+                  (map #(mod (int %) 10)))
+        delta (map - (rest ones) (butlast ones))]
+    delta))
 
 (defn part1
   [f]
-  (let [data (->> f
-                  util/read-lines
-                  (map edn/read-string))]
-    (->> data
-         (map (partial iterate-secrets 2000))
-         (reduce +))))
+  (let [numbers (read-data f)]
+    (transduce
+     (map (comp last (partial iterate-secrets 2000)))
+     +
+     numbers)))
+
+(defn part2
+  [f]
+  (let [numbers (read-data f)
+        ones (->> numbers
+                  (map find-opt-seq))]
+    ones))
 
 (comment
   (def testf "data/day22-test.txt")
   (def inputf "data/day22-input.txt")
   (part1 testf)
-  (part1 inputf)
-  #_(part2 testf)
+  (time (part1 inputf))
+  (time (part2 testf))
   #_(part2 inputf))
