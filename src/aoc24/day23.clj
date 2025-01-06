bbc37a6a6dfdc09cd8243be80cfc6b8b570cf307
alphajuliet <andrew@alphajuliet.com>
2025-01-06 14:11:19 +1100

    Solve 23.1

---
+++ src/aoc24/day23.clj
@@ -0,0 +1,50 @@
+(ns aoc24.day23 
+  (:require
+   [aoc24.util :as util]
+   [clojure.set :as set]
+   [clojure.string :as str]))
+
+(defn read-data
+  [f]
+  (->> f
+       util/read-lines
+       (map (partial re-seq #"\w{2}"))))
+
+(defn create-net
+  "Create a hash map of each node's neighbours"
+  [conns]
+  (reduce (fn [net [a b]]
+            (-> net
+                (assoc a (conj (get net a #{}) b))
+                (assoc b (conj (get net b #{}) a))))
+          {}
+          conns))
+
+(defn find-subnets
+  "Find all subnets of size 3 in a graph where graph is a map of sets"
+  [graph]
+  (->> (for [[v1 neighbors] graph
+             v2 neighbors
+             :when (neg? (compare v1 v2))  ; Ensure v1 < v2 lexicographically
+             :let [common-neighbors (set/intersection neighbors (get graph v2))]
+             v3 common-neighbors
+             :when (neg? (compare v2 v3))] ; Ensure v2 < v3 lexicographically
+         #{v1 v2 v3})
+       (into #{})))
+
+(defn part1
+  [f]
+  (let [data (read-data f)]
+    (->> data
+         create-net
+         find-subnets
+         (map #(apply str %))
+         (util/count-if #(re-matches #"t.....|..t...|....t." %)))))
+
+(comment
+  (def testf "data/day23-test.txt")
+  (def inputf "data/day23-input.txt")
+  (part1 testf)
+  (part1 inputf))
+
+;; The End
