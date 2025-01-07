155901011a62e25a5272bc673507f947a1886c63
alphajuliet <andrew@alphajuliet.com>
2025-01-07 11:55:11 +1100

    Solve 25.1

---
+++ src/aoc24/day25.clj
@@ -0,0 +1,52 @@
+(ns aoc24.day25 
+  (:require
+   [clojure.string :as str]
+   [aoc24.util :as util]))
+
+(defn get-size
+  "Count the number of hash characters in a string"
+  [s]
+  (->> s
+       (filter #(= \# %))
+       count))
+
+(defn read-data
+  [f]
+  (let [blocks (-> f slurp (str/split #"\n\n"))
+        schems (map str/split-lines blocks)
+        locks (->> schems
+                   (filter #(= (first %) "#####"))
+                   (map util/T)
+                   (util/mapmap get-size))
+        keys (->> schems
+                  (filter #(= (first %) "....."))
+                  (map util/T)
+                  (util/mapmap get-size))]
+    [locks keys]))
+
+(defn check-fit?
+  [v1 v2]
+  (every? #(<= % 5) (map + v1 v2)))
+
+(defn find-fit
+  [locks keys]
+  (for [i (range (count locks))
+        j (range (count keys))
+        :when (check-fit? (locks i) (keys j))]
+    [i j]))
+
+(defn part1
+  [f]
+  (->> f
+       read-data
+       (apply find-fit)
+       count))
+
+(comment
+  (def testf "data/day25-test.txt")
+  (def inputf "data/day25-input.txt")
+  
+  (part1 testf)
+  (part1 inputf))
+
+;; The End
