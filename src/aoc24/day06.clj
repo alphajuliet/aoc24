761e250c48d883f7184eee281032615bf91996f3
alphajuliet <andrew@alphajuliet.com>
2025-01-12 09:58:41 +1100

    Save updated approach to 6.2

--- src/aoc24/day06.clj
+++ src/aoc24/day06.clj
@@ -1,7 +1,6 @@
 (ns aoc24.day06
-  (:require
-   [aoc24.util :as util]
-   [clojure.set :as set]))
+  (:require [aoc24.util :as util]
+            [clojure.set :as set]))
 
 (defn occurrences
   "Return the locations of all occurrences of the element in the collection"
@@ -22,12 +21,13 @@
   (let [lines (util/read-lines f)
         rows (count lines)
         cols (count (first lines))
-        guard {:loc (first (find-item lines \^)) :dir [-1 0]}]
+        guard {:loc (first (find-item lines \^)) :dir [-1 0]}] 
     {:dims [rows cols]
-     :distance 0
+    ;;  :distance 0
      :trail (vector guard)
      :obstacles (find-item lines \#)
-     :guard guard}))
+     :guard guard
+     :exit-status :ok}))
 
 (defn turn-right
   [[dr dc]]
@@ -42,73 +42,47 @@
       ;; else
       (-> state
           (assoc-in [:guard :loc] next-loc)
-          (update :trail conj {:loc next-loc :dir (:dir guard)})
-          (update :distance inc)))))
+          (update :trail conj {:loc next-loc :dir (:dir guard)})))))
 
 (defn traverse-room
-  "Traverse the room until we leave or run out of time"
-  [{:keys [dims] :as state}]
+  "Traverse the room until we leave, run out of time, or revisit any location+direction"
+  [state]
   (reduce (fn [st _]
             (let [st' (move-guard st)
-                  [rmax cmax] dims
-                  [r c] (get-in st' [:guard :loc])]
-              (if (or (neg-int? r) (neg-int? c)
-                      (>= r rmax) (>= c cmax))
-                (reduced st')
-                st')))
+                  [rows cols] (:dims st') 
+                  current-loc-dir (:guard st')
+                  [r c] (:loc current-loc-dir)]
+              (cond 
+                (or (neg-int? r) (neg-int? c) (>= r rows) (>= c cols))
+                  (-> st'
+                      (assoc :exit-status :escaped)
+                      reduced)
+                (some #{current-loc-dir} (butlast (:trail st')))
+                  (-> st'
+                      (assoc :exit-status :looped)
+                      reduced)
+                :else st')))
           state
           (range 10000)))
 
-(defn extend-segment
-  "Extend the segment forwards and backwards until it hits an obstacle coordinate or the edge of the room, 
-   and return the new start and end locations"
-  [[rmax cmax] obstacles {:keys [start end dir] :as segment}]
-  (let [start' (take-while (fn [[r c :as loc]]
-                             (and (<= 0 r rmax) (<= 0 c cmax)
-                                  (not (some #(= % loc) obstacles))))
-                           (iterate #(mapv - % dir) start))
-        end' (take-while (fn [[r c :as loc]]
-                           (and (<= 0 r rmax) (<= 0 c cmax)
-                                (not (some #(= % loc) obstacles))))
-                         (iterate #(mapv + % dir) end))]
-    (assoc segment :a (last start') :z (last end'))))
+(defn- test-for-loop
+  "Test for a loop from turning right at step i in the trail"
+  [state i]
+  (let [{:keys [loc dir]} (nth (:trail state) i)]
+    (-> state
+        (update :trail #(vec (take (inc i) %))) ;; truncate trail to the current position
+        (update :obstacles conj (mapv + loc dir)) ;; add an obstacle
+        (assoc :guard {:loc loc :dir dir})
+        traverse-room)))
 
-(defn find-vectors
-  "Find all the segments and their extensions"
-  [{:keys [dims obstacles trail]}]
-  (let [segments (partition-by :dir (butlast trail))]
-    (for [s segments
-          :let [seg {:start (:loc (first s))
-                     :end (:loc (last s))
-                     :dir (:dir (first s))}]]
-      (extend-segment (mapv dec dims) obstacles seg))))
-
-(defn all-coords
-  "List all the coords from start to end exclusive, in the given direction"
-  [start end dir]
-  (take-while #(not= % end)
-              (iterate #(mapv + % dir) start)))
-
-(defn find-intersection
-  "Return any intersection between the segment and the vector in the right directions"
-  [s v]
-  (let [[dr dc :as dir1] (:dir s)
-        dir2 (:dir v)
-        intersect (set/intersection (set (all-coords (:start s) (:end s) dir1))
-                                    (set (all-coords (:a v) (:z v) dir2)))]
-    (when (and intersect (= dir2 [dc (- dr)]))
-      (first intersect))))
-
-(defn find-intersections
-  "Find all the intersections of a segment between :start and :end and previous vector between :a and :z,
-   and return the location of the new obstacle."
-  [segments]
-  (for [v (range (count segments))
-        s (range (inc v) (count segments))
-        :when (< v s)
-        :let [intersect (find-intersection (nth segments s) (nth segments v))]
-        :when intersect]
-    (mapv + (:dir (nth segments s)) intersect)))
+(defn find-loops
+  "Given the guard's trail, at each step check if turning right and traversing the room 
+   results in revisiting a location and direction"
+  [state]
+  (for [i (range 1 (dec (count (:trail state))))
+        :let [st (test-for-loop state i)]
+        :when (= :looped (:exit-status st))]
+    (nth (:trail st) i)))
 
 (defn part1
   [f]
@@ -118,26 +92,28 @@
        (:trail)
        (map :loc)
        set
-       count
+       count 
        dec))
 
 (defn part2
   [f]
-  (let [result (->> f 
-                    read-data 
-                    traverse-room)]
-    (->> result
-         find-vectors
-         find-intersections
-         count)))
+  (->> f
+       read-data
+       traverse-room
+       find-loops
+       count))
 
 (comment
   (def testf "data/day06-test.txt")
+  (def test2f "data/day06-test2.txt")
   (def inputf "data/day06-input.txt")
   (part1 testf)
+  (part1 test2f)
   (part1 inputf)
   (part2 testf)
+  (part2 test2f)
   (part2 inputf))
-;; 371 is too low!
+;; 371,372,373 are too low!
+;; 1738 is incorrect
 
 ;; The End
